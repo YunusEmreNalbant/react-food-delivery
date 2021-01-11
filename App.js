54d6ba5af2bba8c18829a9280a5b7c5a795bcf19
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-01-11 15:17:47 +0300

    bottom tab olusturuldu.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {Restaurant, OrderDelivery, Home} from './screens';
+import Tabs from './navigation/tabs';
 
 
 const Stack = createStackNavigator();
@@ -15,7 +16,7 @@ const App = () => {
               }}
               initialRouteName={"Home"}
             >
-              <Stack.Screen name="Home" component={Home} />
+              <Stack.Screen name="Home" component={Tabs} />
               <Stack.Screen name="Restaurant" component={Restaurant} />
               <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
             </Stack.Navigator>
