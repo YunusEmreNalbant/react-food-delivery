862e53f3cd2956db17f24daad8b497f7fbec7d02
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-01-14 16:19:46 +0300

    restaurant sayfası yapılmaya baslandı

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet, TouchableOpacity, Image, Animated} from 'react-native';
+import {isIphoneX} from 'react-native-iphone-x-helper';
+import {icons, COLORS, SIZES, FONTS} from '../constants';
 
-const Restaurant = () => {
+const Restaurant = ({route, navigation}) => {
+    const [restaurant, setRestaurant] = React.useState(null);
+    const [currentLocation, setCurrentLocation] = React.useState(null);
+
+    React.useEffect(() => {
+        let {item, currentLocation} = route.params;
+        setRestaurant(item);
+        setCurrentLocation(currentLocation);
+    });
+
+
+    function renderHeader() {
+        return (
+            <View style={{flexDirection: 'row'}}>
+                <TouchableOpacity
+                    style={{width: 50, padding: SIZES.padding * 2, justifyContent: 'center'}}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Image source={icons.back} resizeMode={'contain'} style={{width: 30, height: 30}}/>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
     return (
-        <View>
-            <Text>Restaurant</Text>
+        <SafeAreaView style={styles.container}>
+            {renderHeader()}
 
-        </View>
+        </SafeAreaView>
     );
 };
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.lightGray2,
+    },
+
+});
 export default Restaurant;
